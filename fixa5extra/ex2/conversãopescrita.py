4488849f4326a6490e10c32fffc684ec5a8f3e64
Rodrigo D <neonrok@pm.me>
2023-11-06 17:11:07 +0000

    ex 2 feito

---
+++ fixa5extra/ex2/conversãopescrita.py
@@ -0,0 +1,31 @@
+def convertText(X):
+    S=""
+    for i in range(len(X)):
+        Letra=X[i]
+        
+        if Letra=="1":
+            S+="um"
+        elif Letra=="2":
+            S+="dois"
+        elif Letra=="3":
+            S+="tres"
+        elif Letra=="4":
+            S+="quatro"
+        elif Letra=="5":
+            S+="cinco"
+        elif Letra=="6":
+            S+="seis"
+        elif Letra=="7":
+            S+="sete"
+        elif Letra=="8":
+            S+="oito"
+        elif Letra=="9":
+            S+="nove"
+        elif Letra=="0":
+            S+="zero"
+        else:
+            S+=Letra
+    return(S)
+
+nome=str(input("escrevai\n>> "))
+print(convertText(nome))
